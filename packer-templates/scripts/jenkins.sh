9a920f984edfc3bf9c4d5075bf63deaa6c533e20
josehrami <josehr@gmail.com>
2017-06-05 16:01:18 -0700

    fixing jenkins script adding dos2unix

--- packer-templates/scripts/jenkins.sh
+++ packer-templates/scripts/jenkins.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -eux
 
 # JDK and JRE are required for Jenkins
-apt-get install -y openjdk-7-jre openjdk-7-jdk unzip dos2unix
+#apt-get install -y openjdk-7-jre openjdk-7-jdk unzip dos2unix
+
+add-apt-repository ppa:openjdk-r/ppa
+apt-get update
+apt-get install openjdk-8-jre openjdk-8-jre-headless openjdk-8-jdk
 
 wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
 echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list
@@ -10,12 +14,16 @@ apt-get update
 apt-get install -y jenkins
 apt-get upgrade
 
+
+apt-get install dos2unix
 # copy premade configuration files
 # jenkins default config, to set --prefix=jenkins
 cp -f /tmp/jenkins-config/jenkins /etc/default
 # fix dos newlines for Windows users
 dos2unix /etc/default/jenkins
 # install some extra plugins
+dos2unix /tmp/jenkins-config/install_jenkins_plugins.sh
+chmod +x /tmp/jenkins-config/install_jenkins_plugins.sh
 /bin/bash /tmp/jenkins-config/install_jenkins_plugins.sh
 # jenkins security and pipeline plugin config
 cp -f /tmp/jenkins-config/config.xml /var/lib/jenkins
